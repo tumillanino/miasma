b78f17e8d0d18f78b4a26ac43bf4931be8ef8150
tumillanino <anthony.tumia@live.com.au>
2026-04-02 15:50:48 +1100

    shell scripts already migrated

--- assets/daemonix-shells/custom.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# custom name
-sed -i 's/^NAME=.*/NAME="Fedora (Daemonix)"/' /etc/os-release
-sed -i 's/^PRETTY_NAME=.*/PRETTY_NAME="Fedora (Daemonix)"/' /etc/os-release
--- assets/daemonix-shells/extra-rpms.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Docker Desktop
-dnf install -y https://desktop.docker.com/linux/main/amd64/docker-desktop-x86_64.rpm
